1fcf4bd13334fbdc40d4de0554c95d2e614eb64b
diegoy809 <dterra809@gmail.com>
2026-05-26 13:14:12 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCF7aNPFHVr-PwOiQD-xvakja4nm4iLNZo",
-  authDomain: "sushi-890bf.firebaseapp.com",
-  databaseURL: "https://sushi-890bf-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "sushi-890bf",
-  storageBucket: "sushi-890bf.firebasestorage.app",
-  messagingSenderId: "581156208916",
-  appId: "1:581156208916:web:e38536461312caf37b8201"
+  apiKey: "AIzaSyBWKDKAWvSt8ukmW6YtgxGHoK4HQczTia4",
+  authDomain: "dongbeishaokao-cfc3d.firebaseapp.com",
+  databaseURL: "https://dongbeishaokao-cfc3d-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "dongbeishaokao-cfc3d",
+  storageBucket: "dongbeishaokao-cfc3d.firebasestorage.app",
+  messagingSenderId: "509212341963",
+  appId: "1:509212341963:web:a56019fb18674ba0320d4e"
 };
 
 const app = initializeApp(firebaseConfig);
